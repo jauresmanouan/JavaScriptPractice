81142bf1bc2f5017f170c885038aee42f3121245
jauresmanouan <jauresmanouan@gmail.com>
2024-01-04 14:49:53 +0000

    Exo 8

---
+++ 8.js
@@ -0,0 +1,12 @@
+/**
+ * Utilisez des opérateurs bit à bit pour inverser les bits d'un nombre entier.
+ * Affichez le résultat en binaire.
+ */
+
+function inverserBits (nombre) {
+    let entier = nombre;
+    console.log(entier.toString(2));
+}
+
+let a = 45;
+inverserBits(a);
